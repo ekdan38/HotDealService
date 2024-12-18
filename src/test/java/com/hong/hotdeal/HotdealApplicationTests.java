1d6a68f9728a34391d1f3bb559f555e2d4ceba12
JeongHwa <ekdan31@gmail.com>
2024-12-18 20:48:44 +0900

    init

---
+++ src/test/java/com/hong/hotdeal/HotdealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.hotdeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotdealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
