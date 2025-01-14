0c155f6bc489f123f2c25882251fc4d8ddd2bfef
JeongHwa <ekdan31@gmail.com>
2025-01-14 23:25:03 +0900

    Comment : AuthController 주석 수정

--- user-service/src/main/java/com/hong/userservice/web/AuthController.java
+++ user-service/src/main/java/com/hong/userservice/web/AuthController.java
@@ -1,7 +1,6 @@
 package com.hong.userservice.web;
 
 import com.hong.common.dto.ResponseDto;
-import com.hong.common.dto.UserDto;
 import com.hong.userservice.service.UserService;
 import com.hong.userservice.dto.SignupResponseDto;
 import com.hong.userservice.web.dto.EmailVerificationRequestDto;
@@ -11,7 +10,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -49,7 +47,7 @@ public class AuthController {
     @PostMapping("/verify-email")
     public ResponseEntity<?> verifyEmail(@RequestBody @Validated EmailVerifyRequestDto requestDto,
                                          BindingResult bindingResult) {
-        // code 받고 검사
+        // 요청 dto 오류 검사
         if (bindingResult.hasErrors()) {
             log.error("이메일 인증 검사 요청 오류 = {}", bindingResult);
             return ResponseEntity.badRequest().body(bindingResult);
@@ -67,7 +65,7 @@ public class AuthController {
     public ResponseEntity<?> signup(@RequestBody @Validated SignupRequestDto requestDto,
                                     BindingResult bindingResult) {
 
-        // signupRequestDto 필드 에러
+        // 요청 dto 오류 검사
         if (bindingResult.hasErrors()) {
             log.error("회원 가입 요청 검사 오류 = {}", bindingResult);
             return ResponseEntity.badRequest().body(bindingResult);
