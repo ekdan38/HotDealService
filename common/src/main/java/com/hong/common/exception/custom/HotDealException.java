94075d3908259c5ab2d3c32e865bfa00520cc5c5
JeongHwa <ekdan31@gmail.com>
2025-01-17 12:54:45 +0900

    Feat : HotDealException 생성
    앞선 commit 누락

---
+++ common/src/main/java/com/hong/common/exception/custom/HotDealException.java
@@ -0,0 +1,9 @@
+package com.hong.common.exception.custom;
+
+import com.hong.common.exception.ErrorCode;
+
+public class HotDealException extends BusinessException{
+    public HotDealException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
