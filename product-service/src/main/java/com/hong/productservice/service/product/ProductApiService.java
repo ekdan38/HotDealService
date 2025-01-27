094ab8223671e5953bec50e66fe544a1c7b3bc72
JeongHwa <ekdan31@gmail.com>
2025-01-27 20:43:00 +0900

    Refactor : 예외 처리 로직 acquireLocks 메서드 내부로 캡슐화
    acquireLoks 메서드 내부에서 InterruptedException 처리하도록 수정
    
    커밋 오류로 누락된 getProuctByIds service로직, controller 생성

--- product-service/src/main/java/com/hong/productservice/service/product/ProductApiService.java
+++ product-service/src/main/java/com/hong/productservice/service/product/ProductApiService.java
@@ -35,56 +35,64 @@ public class ProductApiService {
         return productRepository.findById(productId)
                 .orElseThrow(() -> new ProductException(ErrorCode.PRODUCT_NOT_FOUND));
     }
+    //  products 조회
+    public List<ProductCommonDto> getProductsByIds(List<Long> productIds) {
+        List<Product> products = productRepository.findAllByProductIds(productIds);
+        if (productIds.size() != products.size()) {
+            throw new ProductException(ErrorCode.PRODUCT_NOT_FOUND);
+        }
+        return products.stream()
+                .map(product -> new ProductCommonDto(
+                        product.getId(),
+                        product.getTitle(),
+                        product.getPrice(),
+                        product.getStock()))
+                .collect(Collectors.toList()
+                );
+
+    }
 
     // product 재고 감소
     @Transactional
-    public List<ProductCommonDto> decreaseStock(List<ProductCommonDto> productCommonDtos){
+    public List<ProductCommonDto> decreaseStock(List<ProductCommonDto> productCommonDtos) {
         List<ProductCommonDto> responseDtos;
-        try{
-            // productIds 추출
-            List<Long> productIds = extractProductIds(productCommonDtos);
-            // 락 획득
-            List<RLock> locks = acquireLocks(productIds);
-            // proudcts 조회, 검증
-            List<Product> products = fetchAndValidate(productCommonDtos);
-            // 재고 감소
-            responseDtos = decreaseStock(productCommonDtos, products);
-            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
-                @Override
-                public void afterCommit() {
-                    releaseLocks(locks);
-                }
-            });
-        }catch (InterruptedException e){
-            Thread.currentThread().interrupt();
-            throw new HotDealException(ErrorCode.PRODUCT_LOCK_FAILED);
-        }
+        // productIds 추출
+        List<Long> productIds = extractProductIds(productCommonDtos);
+        // 락 획득
+        List<RLock> locks = acquireLocks(productIds);
+        // proudcts 조회, 검증
+        List<Product> products = fetchAndValidate(productCommonDtos);
+        // 재고 감소
+        responseDtos = decreaseStock(productCommonDtos, products);
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                releaseLocks(locks);
+            }
+        });
         return responseDtos;
     }
 
     // product 재고 증가
     @Transactional
-    public List<ProductCommonDto> increaseStock(List<ProductCommonDto> productCommonDtos){
+    public List<ProductCommonDto> increaseStock(List<ProductCommonDto> productCommonDtos) {
         List<ProductCommonDto> responseDtos;
-        try{
-            // productIds 추출
-            List<Long> productIds = extractProductIds(productCommonDtos);
-            // 락 획득
-            List<RLock> locks = acquireLocks(productIds);
-            // proudcts 조회, 검증
-            List<Product> products = fetchAndValidate(productCommonDtos);
-            // 재고 감소
-            responseDtos = increaseStock(productCommonDtos, products);
-            TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
-                @Override
-                public void afterCommit() {
-                    releaseLocks(locks);
-                }
-            });
-        }catch (InterruptedException e){
-            Thread.currentThread().interrupt();
-            throw new HotDealException(ErrorCode.PRODUCT_LOCK_FAILED);
-        }
+
+        // productIds 추출
+        List<Long> productIds = extractProductIds(productCommonDtos);
+        // 락 획득
+        List<RLock> locks = acquireLocks(productIds);
+        // proudcts 조회, 검증
+        List<Product> products = fetchAndValidate(productCommonDtos);
+        // 재고 감소
+        responseDtos = increaseStock(productCommonDtos, products);
+        TransactionSynchronizationManager.registerSynchronization(new TransactionSynchronization() {
+            @Override
+            public void afterCommit() {
+                releaseLocks(locks);
+            }
+        });
+
         return responseDtos;
     }
 
@@ -128,7 +136,7 @@ public class ProductApiService {
                 .sorted()
                 .collect(Collectors.toList());
         List<Product> products = productRepository.findAllByProductIds(productIds);
-        if(products.size() != productIds.size()){
+        if (products.size() != productIds.size()) {
             log.error("존재하지 않는 상품이 포함되어 있습니다.");
             throw new ProductException(ErrorCode.PRODUCT_NOT_FOUND);
         }
@@ -136,13 +144,13 @@ public class ProductApiService {
     }
 
     // productIds 추출
-    private List<Long> extractProductIds(List<ProductCommonDto> productCommonDtos){
+    private List<Long> extractProductIds(List<ProductCommonDto> productCommonDtos) {
         return productCommonDtos.stream().map(ProductCommonDto::getId)
                 .collect(Collectors.toList());
     }
 
     // 락 획득
-    private List<RLock> acquireLocks(List<Long> productIds) throws InterruptedException {
+    private List<RLock> acquireLocks(List<Long> productIds) {
         // 락 객체 목록 생성
         List<RLock> locks = new ArrayList<>();
         // 상품 락 생성
@@ -150,21 +158,27 @@ public class ProductApiService {
             String lockKey = "product_lock:" + productId;
             log.info("락 획득 시도 key = {}", lockKey);
             RLock lock = redissonClient.getLock(lockKey);
-            boolean isLocked = lock.tryLock(10L, 10L, TimeUnit.SECONDS);
-            if(!isLocked) {
-                log.error("락 획득 실패 key = {} ", lockKey);
-                throw new HotDealException(ErrorCode.PRODUCT_LOCK_FAILED);
+            try {
+                boolean isLocked = lock.tryLock(10L, 10L, TimeUnit.SECONDS);
+                if (!isLocked) {
+                    log.error("락 획득 실패 key = {} ", lockKey);
+                    throw new ProductException(ErrorCode.PRODUCT_LOCK_FAILED);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new ProductException(ErrorCode.PRODUCT_LOCK_FAILED);
             }
             locks.add(lock);
             log.info("락 획득 성공 key = {}", lockKey);
         }
         return locks;
     }
+
     // 락 해제
     private void releaseLocks(List<RLock> locks) {
         for (RLock lock : locks) {
             String lockKey = lock.getName();
-            if(lock != null && lock.isHeldByCurrentThread()){
+            if (lock != null && lock.isHeldByCurrentThread()) {
                 lock.unlock();
                 log.info("락 해제 key = {}", lockKey);
             }
--- product-service/src/main/java/com/hong/productservice/web/controller/ProductApiController.java
+++ product-service/src/main/java/com/hong/productservice/web/controller/ProductApiController.java
@@ -1,16 +1,11 @@
 package com.hong.productservice.web.controller;
 
 import com.hong.common.dto.ProductCommonDto;
-import com.hong.productservice.dto.product.ProductDto;
 import com.hong.productservice.service.product.ProductApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +14,12 @@ import java.util.List;
 public class ProductApiController {
 
     private final ProductApiService productApiService;
+    @GetMapping("/products")
+    ResponseEntity<List<ProductCommonDto>> getProductsById(@RequestParam List<Long> productIds) {
+
+        List<ProductCommonDto> productsByIds = productApiService.getProductsByIds(productIds);
+        return ResponseEntity.ok().body(productsByIds);
+    }
 
     @PostMapping("/products/decrease-stock")
     ResponseEntity<List<ProductCommonDto>> fetchAndDecreaseStock(@RequestBody List<ProductCommonDto> productCommonDtos) {
