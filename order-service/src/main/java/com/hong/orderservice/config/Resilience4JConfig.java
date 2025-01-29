5900c6ab5266b2d19c6ca60eeddfd54713031849
JeongHwa <ekdan31@gmail.com>
2025-01-30 00:05:27 +0900

    Refactor : circuitBreaker 무시할 예외 지정
    HotDealException, HotDealProductException 무시할 예외 지정 => errorDecoder 에서 응답 처리

--- order-service/src/main/java/com/hong/orderservice/config/Resilience4JConfig.java
+++ order-service/src/main/java/com/hong/orderservice/config/Resilience4JConfig.java
@@ -1,5 +1,7 @@
 package com.hong.orderservice.config;
 
+import com.hong.common.exception.custom.HotDealException;
+import com.hong.common.exception.custom.HotDealProductException;
 import feign.FeignException;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
@@ -54,6 +56,8 @@ public class Resilience4JConfig {
                 .slowCallDurationThreshold(Duration.ofSeconds(2))
                 // 느린 호출이 50% 이상이면 실패로 간주
                 .slowCallRateThreshold(50)
+                // 제외할 예외(errorDecoder 처리)
+                .ignoreExceptions(HotDealProductException.class, HotDealException.class)
                 .build();
         return CircuitBreakerRegistry.of(circuitBreakerConfig);
 
