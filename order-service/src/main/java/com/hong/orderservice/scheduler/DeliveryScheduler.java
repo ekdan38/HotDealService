5934a981826d0e61537adc52d4d1b185004ef2c8
JeongHwa <ekdan31@gmail.com>
2025-02-01 16:37:18 +0900

    Refactor : DeliveryScheduler 수정
    log, 로직 수정

--- order-service/src/main/java/com/hong/orderservice/scheduler/DeliveryScheduler.java
+++ order-service/src/main/java/com/hong/orderservice/scheduler/DeliveryScheduler.java
@@ -2,13 +2,12 @@ package com.hong.orderservice.scheduler;
 
 import com.hong.common.exception.ErrorCode;
 import com.hong.common.exception.custom.BusinessException;
-import com.hong.common.exception.custom.HotDealProductException;
-import com.hong.orderservice.domain.status.DeliveryStatus;
 import com.hong.orderservice.repository.DeliveryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +24,7 @@ public class DeliveryScheduler {
 
     private final DeliveryRepository deliveryRepository;
     private final RedissonClient redissonClient;
+    private final Environment env;
 
     // 4 시간 마다 실행
     // 어떠한 인스턴스가 스케쥴링을 시작하면 다른 인스턴스는 스케쥴링을 돌리지 않도록 한다.
@@ -33,33 +33,30 @@ public class DeliveryScheduler {
     @Scheduled(fixedRate = 14400000)
     @Transactional
     public void updateDeliveryStatus(){
+        String instanceId = env.getProperty("eureka.instance.instance-id");
         String lockKey = "delivery_status_lock:";
         RLock lock = redissonClient.getLock(lockKey);
         try {
             boolean isLocked = lock.tryLock(0, -1, TimeUnit.SECONDS);
             if (!isLocked) {
-                log.debug("delivery_status 에 대한 락 획득에 실패했습니다.");
-                throw new BusinessException(ErrorCode.DELIVERY_LOCK_FAILED);
+                log.info("다른 인스턴스가 스케쥴링을 진행중입니다.");
             }
             log.info("락 획득 성공 key = {}", lockKey);
+            log.info("{} 인스턴스가 스케쥴링을 진행합니다.", instanceId);
 
             LocalDateTime now = LocalDateTime.now();
 
             // 주문 후 1일 경과한 배송 DELIVERING 로 상태 변경
-            deliveryRepository.updatePendingDeliveriesToDelivering(
+            deliveryRepository.bulkUpdatePendingDeliveriesToDelivering(
                     now.minusDays(1),
-                    DeliveryStatus.DELIVERING,
-                    LocalDateTime.now(),
-                    DeliveryStatus.PENDING
+                    LocalDateTime.now()
             );
             log.info("Scheduler DELIVERING 로 상태 변경");
 
-            // 주문 후 2일 경과한 배송 DELIVERED 로 상태 변경
-            deliveryRepository.updatePendingDeliveriesToDelivering(
-                    now.minusDays(2),
-                    DeliveryStatus.DELIVERING,
-                    LocalDateTime.now(),
-                    DeliveryStatus.DELIVERED
+            // 배송 시작 후 1일 경과한 배송 DELIVERED 로 상태 변경
+            deliveryRepository.bulkUpdateDeliveringDeliveriesToDelivered(
+                    now.minusDays(1),
+                    LocalDateTime.now()
             );
             log.info("Scheduler DELIVERED 로 상태 변경");
 
@@ -67,8 +64,11 @@ public class DeliveryScheduler {
                 @Override
                 public void afterCommit() {
                     lock.unlock();
+                    log.info("락 해제 완료 key = {}", lockKey);
                 }
             });
+            log.info("{} 인스턴스가 스케쥴링을 종료 했습니다.", instanceId);
+
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             log.debug("delivery_status 에 대한 락 획득 중 입터럽트가 발생했습니다.");
