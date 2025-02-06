a9849ad549969c19c5594ae40f0a14972801104c
JeongHwa <ekdan31@gmail.com>
2025-02-06 23:11:40 +0900

    Feat : paymentService PaymentServiceApplication 필요한 어노테이션 작성
    common 모듈 스캔, jpaAuditing, feignClients

--- payment-service/src/main/java/com/hong/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/hong/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,14 @@ package com.hong.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.hong.paymentservice", "com.hong.common"})
+@EnableJpaAuditing
+@EnableFeignClients
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
