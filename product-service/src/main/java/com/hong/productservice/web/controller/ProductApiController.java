da0ad0b6b50e3f7fb98ea69f4ef2a77bb2e5a8e7
JeongHwa <ekdan31@gmail.com>
2025-02-18 18:10:15 +0900

    Refactor : productService ProductApiController 사용하지 않는 메서드 삭제

--- product-service/src/main/java/com/hong/productservice/web/controller/ProductApiController.java
+++ product-service/src/main/java/com/hong/productservice/web/controller/ProductApiController.java
@@ -15,13 +15,6 @@ public class ProductApiController {
 
     private final ProductApiService productApiService;
 
-    @GetMapping("/products")
-    ResponseEntity<List<ProductDto>> getProductsById(@RequestParam List<Long> productIds) {
-
-        List<ProductDto> productsByIds = productApiService.getProductsByIds(productIds);
-        return ResponseEntity.ok().body(productsByIds);
-    }
-
     @PostMapping("/products")
     ResponseEntity<List<ProductStockCheckResponseDto>> fetchProducts(@RequestBody List<ProductStockCheckRequestDto> requestDtos) {
         List<ProductStockCheckResponseDto> responseDtos = productApiService.fetchProductAndValidateStock(requestDtos);
