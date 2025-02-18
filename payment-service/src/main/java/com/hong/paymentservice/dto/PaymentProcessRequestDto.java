f4df59e106c482e78c031d591013888261b77cd7
JeongHwa <ekdan31@gmail.com>
2025-02-18 18:08:08 +0900

    Feat : paymentService Controller 작성

---
+++ payment-service/src/main/java/com/hong/paymentservice/dto/PaymentProcessRequestDto.java
@@ -0,0 +1,13 @@
+package com.hong.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentProcessRequestDto {
+
+    private Integer userPaymentAmount;
+}
--- payment-service/src/main/java/com/hong/paymentservice/web/PaymentController.java
+++ payment-service/src/main/java/com/hong/paymentservice/web/PaymentController.java
@@ -1,16 +1,44 @@
 package com.hong.paymentservice.web;
 
+import com.hong.common.dto.ResponseDto;
+import com.hong.paymentservice.dto.PaymentEntryResponseDto;
+import com.hong.paymentservice.dto.PaymentProcessRequestDto;
+import com.hong.paymentservice.dto.PaymentProcessResponseDto;
 import com.hong.paymentservice.service.PaymentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j(topic = "[PaymentController]")
+@RequestMapping("/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @PostMapping("/entry/{orderId}")
+    public ResponseEntity<ResponseDto<PaymentEntryResponseDto>> paymentEntry(@RequestHeader("X-User-Id") Long userId,
+                                                                             @PathVariable("orderId") Long orderId) {
+
+        PaymentEntryResponseDto resultDto = paymentService.paymentEntry(userId, orderId);
+
+        // 응답 설정
+        ResponseDto<PaymentEntryResponseDto> responseDto = new ResponseDto<>("결제 진입 완료", resultDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
+
+    @PostMapping("/process/{paymentId}")
+    public ResponseEntity<ResponseDto<PaymentProcessResponseDto>> paymentProcess(@RequestHeader("X-User-Id") Long userId,
+                                                                                 @PathVariable("paymentId") Long paymentId,
+                                                                                 @RequestBody PaymentProcessRequestDto requestDto) {
+
+        PaymentProcessResponseDto resultDto = paymentService.paymentProcess(userId, paymentId, requestDto.getUserPaymentAmount());
+
+        // 응답 설정
+        ResponseDto<PaymentProcessResponseDto> responseDto = new ResponseDto<>("결제 완료", resultDto);
+        return ResponseEntity.ok().body(responseDto);
+    }
 
 }
