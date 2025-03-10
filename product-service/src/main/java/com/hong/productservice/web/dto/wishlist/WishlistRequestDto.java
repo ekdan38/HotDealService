6f9e077e3b5e95287a4e29f0462c92a3deb09891
JeongHwa <ekdan31@gmail.com>
2025-03-11 02:49:20 +0900

    Refactor : ProductService dto 생성자 추가

--- product-service/src/main/java/com/hong/productservice/web/dto/wishlist/WishlistRequestDto.java
+++ product-service/src/main/java/com/hong/productservice/web/dto/wishlist/WishlistRequestDto.java
@@ -1,11 +1,13 @@
 package com.hong.productservice.web.dto.wishlist;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class WishlistRequestDto {
 
     @NotNull(message = "productId 는 필수입니다.")
