b40c95077ac41c3e4919c94e38c371f80af2fff2
JeongHwa <ekdan31@gmail.com>
2025-03-11 17:52:22 +0900

    Refactor : ProductService appConfig 에서 JpaAudting 설정 분리

--- product-service/src/main/java/com/hong/productservice/config/AppConfig.java
+++ product-service/src/main/java/com/hong/productservice/config/AppConfig.java
@@ -3,11 +3,9 @@ package com.hong.productservice.config;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
-@ComponentScan(basePackages = {"com.hong.productservice", "com.hong.common"})
+@ComponentScan(basePackages = "com.hong.common")
 @EnableFeignClients
 public class AppConfig {
 }
