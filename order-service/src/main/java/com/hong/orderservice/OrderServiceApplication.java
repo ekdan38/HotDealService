96bd715a6129c52f304b17ed051834415ed237c8
JeongHwa <ekdan31@gmail.com>
2025-03-19 22:35:58 +0900

    Refactor : OrderService confing 분리

--- order-service/src/main/java/com/hong/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/hong/orderservice/OrderServiceApplication.java
@@ -1,15 +1,12 @@
 package com.hong.orderservice;
 
+import com.hong.orderservice.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.hong.orderservice", "com.hong.common"})
-@EnableJpaAuditing
-@EnableFeignClients
+@Import(AppConfig.class)
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
--- order-service/src/main/java/com/hong/orderservice/config/ClientConfig.java
+++ order-service/src/main/java/com/hong/orderservice/config/ClientConfig.java
@@ -2,10 +2,12 @@ package com.hong.orderservice.config;
 
 import feign.Logger;
 import feign.Retryer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(basePackages = "com.hong.orderservice.client")
 public class ClientConfig {
 
     @Bean
