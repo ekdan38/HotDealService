e20b225d3b4daf991fcf8dbaf32b6ab5c13d1204
홍정화 <ekdan31@gmail.com>
2025-07-01 14:45:47 +0900

    Refactor : PaymentService. Payment Entity 수정.
    변수 수정.
    메서드 수정.
    인덱스 생성.

--- payment-service/src/main/java/com/hong/paymentservice/domain/Payment.java
+++ payment-service/src/main/java/com/hong/paymentservice/domain/Payment.java
@@ -1,19 +1,25 @@
 package com.hong.paymentservice.domain;
 
 import com.hong.paymentservice.domain.base.TimeEntity;
+import com.hong.paymentservice.domain.status.PaymentStatus;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@Table(name = "payment", indexes = @Index(name = "idx_order_id", columnList = "order_id"))
+@Table(name = "payment", indexes = {
+        @Index(name = "idx_order_id", columnList = "order_id"),
+        @Index(name = "idx_user_id", columnList = "user_id")
+})
 public class Payment extends TimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,37 +27,69 @@ public class Payment extends TimeEntity {
     private Long id;
 
     @Column(name = "order_id", nullable = false, unique = true)
-    private Long orderId;
+    private String orderId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 
     @Column(nullable = false)
-    private Integer amount;
+    private BigDecimal amount;
+
+    @Column(nullable = true)
+    private String transactionId;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private PaymentStatus status;
 
+    @Column(nullable = true)
+    private LocalDateTime paidAt;
+
     @Column(nullable = false)
     private LocalDateTime expireAt;
 
-    private Payment(Long orderId, Integer amount) {
+    @Column(nullable = true)
+    private LocalDateTime canceledAt;
+
+    private Payment(String orderId, BigDecimal amount, Long userId, LocalDateTime expireAt) {
         this.orderId = orderId;
         this.amount = amount;
+        this.userId = userId;
         this.status = PaymentStatus.PENDING;
-        expireAt = LocalDateTime.now().plusMinutes(15);
+        this.expireAt = expireAt;
     }
 
     // == 생성 메서드 ==/
-    public static Payment create(Long orderId, Integer amount){
-        return new Payment(orderId, amount);
+    public static Payment create(String orderId, BigDecimal amount, Long userId, LocalDateTime expireAt){
+        return new Payment(orderId, amount, userId, expireAt);
     }
 
-    // == payment 결제 제한 시간 만료 확인 메서드 ==
-    public Boolean expiredPay(){
-        return expireAt.isBefore(LocalDateTime.now());
+    // == payment 결제 성공 변경 메서드 ==
+    public void updateToSuccess(String transactionId){
+        this.transactionId = transactionId;
+        this.status = PaymentStatus.COMPLETED;
+        this.paidAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
-    // == payment 결제 상태 변경 메서드 ==
-    public void updateStatus(PaymentStatus newStatus){
-        this.status = newStatus;
+    // == payment 결제 실패 변경 메서드 ==
+    public void updateToFail(String transactionId){
+        this.transactionId = transactionId;
+        this.status = PaymentStatus.FAILED;
+    }
+
+    // == payment EXPIRED 처리 메서드 ==
+    public void updateToExpired(){
+        this.status = PaymentStatus.EXPIRED;
+    }
+
+    // == payment IN_PROGRESS 처리 메서드 ==
+    public void updateToInProgress(){
+        this.status = PaymentStatus.IN_PROGRESS;
+    }
+
+    // == payment CANCELED 처리 메서드 ==
+    public void updateToCancel(){
+        this.status = PaymentStatus.CANCELED;
+        this.canceledAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 }
---
+++ payment-service/src/main/java/com/hong/paymentservice/domain/status/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.hong.paymentservice.domain.status;
+
+public enum PaymentStatus {
+    PENDING, IN_PROGRESS, COMPLETED, FAILED, EXPIRED, CANCELED
+}
