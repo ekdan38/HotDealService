932a5fe3c546de0a9a2ae5c2929477a0c91d7519
홍정화 <ekdan31@gmail.com>
2025-07-01 11:41:00 +0900

    Refactor : HotDealService. HotDealProduct Entity 수정.
    기존 ProductService 삭제하여 변수 변경 및 전체적인 메서드 수정.

--- hotdeal-service/src/main/java/com/hong/hotdealservice/domain/HotDealProduct.java
+++ hotdeal-service/src/main/java/com/hong/hotdealservice/domain/HotDealProduct.java
@@ -1,21 +1,20 @@
 package com.hong.hotdealservice.domain;
 
-import com.hong.common.exception.ErrorCode;
-import com.hong.common.exception.custom.HotDealProductException;
+import com.hong.hotdealservice.domain.base.TimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import java.math.BigDecimal;
 
 @Entity
-@Slf4j
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class HotDealProduct {
+public class HotDealProduct extends TimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "hotdeal_product_id")
+    @Column(name = "product_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -23,72 +22,52 @@ public class HotDealProduct {
     private HotDeal hotDeal;
 
     @Column(nullable = false)
-    private Long productId;
-
-    // 반정규화
-    @Column(nullable = false)
-    private String productTitle;
-
-    // 반정규화
-    @Column(nullable = false)
-    private Integer originalPrice;
+    private String title;
 
     @Column(nullable = false)
-    private Integer hotDealPrice;
-
-    @Column(nullable = false)
-    private Double discountRate;
+    private BigDecimal price;
 
     @Column(nullable = false)
     private Integer stock;
 
-    private HotDealProduct(Long productId, String productTitle, Integer originalPrice, Double discountRate, Integer stock) {
-        this.productId = productId;
-        this.productTitle = productTitle;
-        this.originalPrice = originalPrice;
-        this.discountRate = discountRate;
+    private HotDealProduct(String title, BigDecimal price, Integer stock) {
+        this.title = title;
+        this.price = price;
         this.stock = stock;
     }
 
     // == 생성 메서드 ==
-    public static HotDealProduct create(Long productId, String productTitle, Integer originalPrice, Double discountRate, Integer stock){
-        HotDealProduct hotDealProduct = new HotDealProduct(productId, productTitle, originalPrice, discountRate, stock);
-        hotDealProduct.setHotDealPrice(discountRate);
-        return hotDealProduct;
+    public static HotDealProduct create(String title, BigDecimal price, Integer stock){
+       return new HotDealProduct(title, price, stock);
     }
 
-    // == HotDeal 에서 연관 관계 설정에 사용할 메서드 ==
+    // == hotDeal 에서 연관 관계 설정에 사용할 메서드 ==
     protected void setHotDeal(HotDeal hotDeal){
         this.hotDeal = hotDeal;
     }
 
-    // == hotDealPrice 설정 메서드 ==
-    private void setHotDealPrice(Double discountRate){
-        this.hotDealPrice = (int) Math.floor(this.originalPrice * (1 - discountRate));
+    // == product Field update 메서드 ==
+    public void updateFields(String title, BigDecimal price, Integer stock){
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
     }
 
     // == stock 감소 메서드 ==
-    public void decreaseStock(Integer quantity){
-        if(this.stock - quantity < 0){
-            log.debug("요청 수량보다 재고가 부족합니다. hotDealProductId = {}, 요청 수량 = {}, 재고 수량 = {}", this.id, quantity, this.stock);
-            throw new HotDealProductException(ErrorCode.HOTDEAL_PRODUCT_INSUFFICIENT_STOCK, this.id, quantity, this.stock);
-        }
+    public boolean decreaseStock(Integer quantity) {
+        if (this.stock - quantity < 0) return false;
         this.stock -= quantity;
-        log.info("재고 감소 성공 hotDealProductId = {}, 차감 수량 = {}, 재고 수량 = {}", this.id, quantity, this.stock);
+        return true;
+    }
+
+    // == stock 조정 메서드 ==
+    public void syncStock(Integer stock){
+        this.stock = stock;
     }
 
     // == stock 증가 메서드 ==
-    public void increaseStock(Integer quantity){
+    public boolean increaseStock(Integer quantity) {
         this.stock += quantity;
-        log.info("재고 증가 성공 hotDealProductId = {}, 증가 수량 = {}, 재고 수량 = {}", this.id, quantity, this.stock);
+        return true;
     }
-
-
-    // == quantity, discountRate update 메서드 ==
-    public void updateQuantityAndDiscountRate(int quantity, double discountRate){
-        this.stock = quantity;
-        this.discountRate = discountRate;
-        setHotDealPrice(discountRate);
-    }
-
 }
