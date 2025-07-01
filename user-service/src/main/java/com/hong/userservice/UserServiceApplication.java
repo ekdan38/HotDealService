1ac4ec33fe2c30125c3853dfba5cbae2d83d6a68
홍정화 <ekdan31@gmail.com>
2025-07-01 14:54:57 +0900

    Refactor : UserService. Config 분리 및 적용.

--- user-service/src/main/java/com/hong/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/hong/userservice/UserServiceApplication.java
@@ -1,14 +1,12 @@
 package com.hong.userservice;
 
+import com.hong.userservice.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@ComponentScan(basePackages = {"com.hong.userservice", "com.hong.common"})
+@Import(AppConfig.class)
 public class UserServiceApplication {
 
     public static void main(String[] args) {
---
+++ user-service/src/main/java/com/hong/userservice/config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.hong.userservice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@ComponentScan(basePackages = "com.hong.common")
+@EnableAsync
+public class AppConfig {
+}
