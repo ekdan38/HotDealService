a733de2030e0ee12bc7e641385862ef85f90e332
홍정화 <ekdan31@gmail.com>
2025-07-01 14:22:11 +0900

    Refactor : OrderService. Resilience4JConfig 수정
    CircuitBreaker 메서드 수정.

--- order-service/src/main/java/com/hong/orderservice/config/Resilience4JConfig.java
+++ order-service/src/main/java/com/hong/orderservice/config/Resilience4JConfig.java
@@ -20,9 +20,8 @@ import java.util.concurrent.TimeoutException;
 @Configuration
 @Slf4j
 public class Resilience4JConfig {
-    private static final int PRIORITY_1 = -3; // Retry의 우선순위
-    private static final int PRIORITY_2 = -4; // CircuitBreaker의 우선순위
-
+    private static final int PRIORITY_1 = -4;
+    private static final int PRIORITY_2 = -3;
     private final CircuitBreakerConfigurationProperties circuitBreakerConfigurationProperties;
     private final RetryConfigurationProperties retryConfigurationProperties;
 
@@ -41,25 +40,26 @@ public class Resilience4JConfig {
     public CircuitBreakerRegistry circuitBreakerRegistry(){
         CircuitBreakerConfig circuitBreakerConfig = CircuitBreakerConfig.custom()
                 //  50% 실패 시 OPEN
-                .failureRateThreshold(10)
-                // OPEN 상태에서 10초 후 HALF_OPEN 으로 전환
-                .waitDurationInOpenState(Duration.ofSeconds(10))
+                .failureRateThreshold(50)
+                // OPEN 상태에서 5초 후 HALF_OPEN 으로 전환
+                .waitDurationInOpenState(Duration.ofSeconds(5))
                 // 카운트 기반의 슬라이딩 윈도우, 20회 호출 기록으로 실패율 계산
                 .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-                .slidingWindowSize(10)
-                // 최소 10건의 호출이 쌓여야 실패율 계산
-                .minimumNumberOfCalls(10)
+                .slidingWindowSize(20)
+                // 최소 20건의 호출이 쌓여야 실패율 계산
+                .minimumNumberOfCalls(20)
                 // HALF_OPEN 상태에서 3건 연속 성공이면 CLOSED 로 전환
                 .permittedNumberOfCallsInHalfOpenState(3)
-                // 2초 이상 걸린 호출을 느린 호출 로 간주
-                .slowCallDurationThreshold(Duration.ofSeconds(2))
-                // 느린 호출이 50% 이상이면 실패로 간주
-                .slowCallRateThreshold(50)
+                // 3초 이상 걸린 호출을 느린 호출 로 간주
+                .slowCallDurationThreshold(Duration.ofSeconds(3))
+                // 느린 호출이 70% 이상이면 실패로 간주
+                .slowCallRateThreshold(70)
                 // 제외할 예외(errorDecoder 처리)
                 .ignoreExceptions(OrderException.class)
                 .build();
-        return CircuitBreakerRegistry.of(circuitBreakerConfig);
-
+        CircuitBreakerRegistry registry = CircuitBreakerRegistry.of(circuitBreakerConfig);
+        registry.circuitBreaker("default", circuitBreakerConfig);
+        return registry;
     }
 
     @Bean
@@ -91,6 +91,7 @@ public class Resilience4JConfig {
                 .onIgnoredError(event -> log.info("[Retry] 무시된 오류: {}, Error: {}",
                         event.getName(),
                         event.getLastThrowable() != null ? event.getLastThrowable().getMessage() : "N/A"));
+        retryRegistry.retry("default", retryConfig);
         return retryRegistry;
 
     }
