f3f4f854862ed4bb9819d1d55354525d86786b8e
홍정화 <ekdan31@gmail.com>
2025-07-01 14:19:01 +0900

    Refactor : OrderService. HotDealService FeignClient URL 수정

--- order-service/src/main/java/com/hong/orderservice/client/hotdeal/HotDealServiceClient.java
+++ order-service/src/main/java/com/hong/orderservice/client/hotdeal/HotDealServiceClient.java
@@ -1,24 +1,23 @@
 package com.hong.orderservice.client.hotdeal;
 
-import com.hong.common.dto.HotDealProductStockCheckRequestDto;
-import com.hong.common.dto.HotDealProductStockCheckResponseDto;
-import com.hong.common.dto.HotDealProductStockUpdateRequestDto;
-import com.hong.common.dto.HotDealProductStockUpdateResponseDto;
+import com.hong.common.dto.*;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import java.util.List;
-
 @FeignClient(name = "hotdeal-service")
 public interface HotDealServiceClient {
 
-    @PostMapping("/hotdeal-service/products")
-    List<HotDealProductStockCheckResponseDto> fetchProductsAndValidateStock(@RequestBody List<HotDealProductStockCheckRequestDto> requestDtos);
+    @PostMapping("/hotdeal-service/products/stock/reserve")
+    ProductReservationResponseDto reserveStock(@RequestBody ProductReservationRequestDto requestDto);
 
-    @PostMapping("/hotdeal-service/decrease-stock")
-    List<HotDealProductStockUpdateResponseDto> decreaseStock(@RequestBody List<HotDealProductStockUpdateRequestDto> requestDtos);
+    @PostMapping("/hotdeal-service/products/stock/finalize")
+    StockFinalizeResponseDto finalizeStockReservation(@RequestBody StockFinalizeRequestDto requestDto);
 
-    @PostMapping("/hotdeal-service/increase-stock")
-    List<HotDealProductStockUpdateResponseDto> increaseStock(@RequestBody List<HotDealProductStockUpdateRequestDto> requestDtos);
+    @PostMapping("/hotdeal-service/products/stock/release")
+    ReleaseReservedStockResponseDto releaseReservedStocks(@RequestBody ReleaseReservedStockRequestDto requestDto);
+
+    @PostMapping("/hotdeal-service/products/stock/restore")
+    StockRestoreResponseDto restoreStock(@RequestBody StockRestoreRequestDto requestDto);
+
 }
