0e7718a5a3890107d1146a6db1d734efefc30ba5
홍정화 <ekdan31@gmail.com>
2025-07-01 14:58:00 +0900

    Refactor : UserService. Repository 수정.
    DtoProjection 적용.

--- user-service/src/main/java/com/hong/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/hong/userservice/repository/UserRepository.java
@@ -1,10 +1,18 @@
 package com.hong.userservice.repository;
 
 import com.hong.userservice.domain.User;
+import com.hong.userservice.dto.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByUsername(String username);
-    User findByUsername(String username);
+
+    @Query("SELECT new com.hong.userservice.dto.UserDto(u.id, u.username, u.password, u.role) " +
+            "FROM User u " +
+            "WHERE u.username = :username")
+    UserDto findDtoByUsername(@Param("username") String username);
+
     boolean existsByEmail(String email);
 }
