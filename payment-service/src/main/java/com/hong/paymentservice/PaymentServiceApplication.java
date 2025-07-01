8d06f51f01686620af7ec78f4344193aac742b1a
홍정화 <ekdan31@gmail.com>
2025-07-01 14:46:58 +0900

    Refactor : PaymentService. 분리한 Config -> Main 적용.

--- payment-service/src/main/java/com/hong/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/hong/paymentservice/PaymentServiceApplication.java
@@ -1,15 +1,12 @@
 package com.hong.paymentservice;
 
+import com.hong.paymentservice.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.hong.paymentservice", "com.hong.common"})
-@EnableJpaAuditing
-@EnableFeignClients
+@Import(AppConfig.class)
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
