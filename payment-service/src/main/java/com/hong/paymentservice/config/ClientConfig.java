6629d7de32f3e388f205153c890f6d44c242c503
홍정화 <ekdan31@gmail.com>
2025-07-01 14:42:27 +0900

    Refactor : PaymentService. ClientConfig 수정.

--- payment-service/src/main/java/com/hong/paymentservice/config/ClientConfig.java
+++ payment-service/src/main/java/com/hong/paymentservice/config/ClientConfig.java
@@ -1,11 +1,12 @@
 package com.hong.paymentservice.config;
 
 import feign.Logger;
-import feign.Retryer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(basePackages = "com.hong.paymentservice.client")
 public class ClientConfig {
 
     @Bean
@@ -13,9 +14,4 @@ public class ClientConfig {
         return Logger.Level.FULL;
     }
 
-    @Bean
-    public Retryer neverRetry() {
-        // spring-retry 비활성화 첫 번째 시도만 시도, 재시도 없음
-        return new Retryer.Default(0, 0, 1);
-    }
 }
