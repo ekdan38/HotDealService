ae4229e16a7f8dcbd16a5e0e5cb97976394215a5
홍정화 <ekdan31@gmail.com>
2025-07-01 14:57:26 +0900

    Refactor : UserService. 인증 관련 로직 수정.
    Dto 사용하도록 수정.

--- user-service/src/main/java/com/hong/userservice/jwt/CustomUserDetailsService.java
+++ user-service/src/main/java/com/hong/userservice/jwt/CustomUserDetailsService.java
@@ -1,12 +1,9 @@
 package com.hong.userservice.jwt;
 
-import com.hong.userservice.AESUtil;
-import com.hong.userservice.domain.User;
 import com.hong.userservice.dto.UserDto;
 import com.hong.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -21,25 +18,18 @@ import java.util.List;
 @Slf4j(topic = "[CustomUserDetailsService]")
 public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
-    private final AESUtil aesUtil;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try {
-            String encryptUsername = aesUtil.encrypt(username);
-            User user = userRepository.findByUsername(encryptUsername);
+            UserDto userDto = userRepository.findDtoByUsername(username);
 
-            if (user == null) {
+            if (userDto == null) {
                 log.error("error = {}", username);
                 throw new UsernameNotFoundException("일치하는 username 이 없습니다.");
             }
-            // userDto로 변환
-            ModelMapper modelMapper = new ModelMapper();
-            UserDto userDto = modelMapper.map(user, UserDto.class);
-
-//            user.setDecryptUsername(aesUtil.decrypt(user.getUsername()));
             List<GrantedAuthority> authorities =
-                    List.of(new SimpleGrantedAuthority(user.getRole().toString()));
+                    List.of(new SimpleGrantedAuthority(userDto.getRole().toString()));
             return new CustomUserDetails(userDto, authorities);
 
         } catch (Exception e) {
