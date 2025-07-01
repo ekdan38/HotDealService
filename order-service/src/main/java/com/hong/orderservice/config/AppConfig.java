2a7c4ed5f4ccba26e44241ddf25924efd06dd49b
홍정화 <ekdan31@gmail.com>
2025-07-01 14:23:16 +0900

    Feat : OrderService. 이벤트 비동기 처리위한 Config.

--- order-service/src/main/java/com/hong/orderservice/config/AppConfig.java
+++ order-service/src/main/java/com/hong/orderservice/config/AppConfig.java
@@ -2,10 +2,12 @@ package com.hong.orderservice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @ComponentScan(basePackages = "com.hong.common")
 @EnableScheduling
+@EnableAsync
 public class AppConfig {
 }
---
+++ order-service/src/main/java/com/hong/orderservice/config/AsyncConfig.java
@@ -0,0 +1,27 @@
+package com.hong.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "EventExecutor")
+    public Executor taskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 최소 스레드
+        executor.setCorePoolSize(10);
+        // 최대 스레드
+        executor.setMaxPoolSize(50);
+        // 큐가 찼을 때만 maxPool 만큼 늘어남
+        executor.setQueueCapacity(10000);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
