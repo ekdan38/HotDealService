7340c17669d12607ec2f868b7361b9b8dfe637ab
홍정화 <ekdan31@gmail.com>
2025-07-01 14:46:16 +0900

    Refactor : PaymentService. Payment Repository 수정.

--- payment-service/src/main/java/com/hong/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/hong/paymentservice/repository/PaymentRepository.java
@@ -2,11 +2,13 @@ package com.hong.paymentservice.repository;
 
 import com.hong.paymentservice.domain.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    Optional<Payment> findByOrderId(@Param("orderId") Long orderId);
+    Optional<Payment> findByOrderIdAndUserId(String orderId, Long userId);
+
+    Optional<Payment> findByOrderId(String orderId);
 }
