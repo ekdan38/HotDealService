f7a9c24e59af285c4713db7e8efd9270797e4e7e
홍정화 <ekdan31@gmail.com>
2025-07-31 17:55:45 +0900

    Refactor : PaymentService KafkaConfig 수정
    kafkaTemplate<String,Object> => <String, String> 수정, ACK 설정 삭제(기본 설정인 enable.idempotence) 사용 위함

--- payment-service/src/main/java/com/hong/paymentservice/config/KafkaConfig.java
+++ payment-service/src/main/java/com/hong/paymentservice/config/KafkaConfig.java
@@ -22,23 +22,22 @@ public class KafkaConfig {
     private final String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         // Outbox 에 String 형태의 Json으로 저장중 따라서 String으로 직렬화
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.ACKS_CONFIG, 1);
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
+    public KafkaTemplate<String, String> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
 
     @Bean
-    public ConsumerFactory<String, Object> consumerFactory() {
+    public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
@@ -51,9 +50,9 @@ public class KafkaConfig {
 
     // @KafkaListener 어노테이션 위한 팩토리
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory() {
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
         // 여러 컨슈머 스레드를 동시에 실행하여 메시지 처리에 동시성을 제공
-        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
         // 정의된 ConsumerFactory를 사용하여 컨테이너 팩토리를 설정
         factory.setConsumerFactory(consumerFactory());
         return factory;
