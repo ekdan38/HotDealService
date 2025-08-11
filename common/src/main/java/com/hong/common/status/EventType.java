24297857c77bf4179be66ff016978e2d78cf98d3
홍정화 <ekdan31@gmail.com>
2025-08-11 23:08:26 +0900

    Refactor : Common. Outbox EventType 수정

--- common/src/main/java/com/hong/common/status/EventType.java
+++ common/src/main/java/com/hong/common/status/EventType.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum EventType {
     PAYMENT_RESULT("payment-result-topic"),
     PAYMENT_CREATE("payment-create-topic"),
-    ORDER_PAYMENT_RESULT("order-payment-result-topic");
+    ORDER_PAYMENT_RESULT("order-payment-result-topic"),
+    EXPIRED_ORDER("expired-order-topic"),
+    REFUND_ORDER("refund-order-topic");
 
     private final String topic;
 
